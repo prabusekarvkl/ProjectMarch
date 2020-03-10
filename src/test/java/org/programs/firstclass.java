ed8889a037cdf653f52b9ddc4480dc5add21a89a
prabusekarvkl <prabusekarvkl@gmail.com>
2020-03-10 12:49:15 +0530

    tcs 2nd time

--- src/test/java/org/programs/firstclass.java
+++ src/test/java/org/programs/firstclass.java
@@ -5,9 +5,9 @@ public class firstclass {
 	public static void main(String[] args) {
 		System.out.println("prabu kannan");
 		System.out.println("tcs tcs");
-		//2nd try
+		// 2nd try
 		System.out.println("123");
-
+		// 2nd try tcs
 	}
 
 }
